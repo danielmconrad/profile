4f52c9911e9c8ab5bcb9e26a1e2a6c9e67eca4b5
Daniel Conrad <daniel.m.conrad@gmail.com>
2023-01-22 12:12:11 -0600

    Adds temporary file for setting up ubuntu

---
+++ install/ubuntu.temp.sh
@@ -0,0 +1,82 @@
+# Ubuntu Setup
+
+```sh
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install git
+```
+
+## Terminal
+
+Dump a profile
+
+```sh
+dconf dump /org/gnome/terminal/legacy/profiles:/ > gnome-terminal-profiles.dconf
+cat gnome-terminal-profiles.dconf
+```
+
+Load a profile
+
+```sh
+dconf load /org/gnome/terminal/legacy/profiles:/ < gnome-terminal-profiles.dconf
+```
+
+Profile contents (name `gnome-terminal-profiles.dconf`)
+
+```sh
+[:b1dcc9dd-5262-4d8d-a863-c897e6d979b9]
+cursor-shape='ibeam'
+scrollback-lines=100000
+visible-name='Conrad'
+```
+
+## Trackpad
+
+```sh
+sudo apt-get -y install gnome-tweaks
+>> Keyboard & Mouse > Mouse > Acceleration Profile > Adaptive
+```
+
+## SSH
+
+Generate
+
+```sh
+ssh-keygen -t ed25519 -C "daniel.m.conrad@gmail.com"
+```
+
+Add Agent
+
+```sh
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_ed25519
+```
+
+# Add to GitHub
+
+```sh
+cat ~/.ssh/id_ed25519.pub
+> Paste into GitHub
+```
+
+=========================================
+
+# Applications
+
+```sh
+sudo snap install docker
+sudo snap install chromium
+sudo snap install code --classic
+sudo snap install slack
+sudo snap install spotify
+```
+
+# Profile
+
+```sh
+sudo snap install chezmoi --classic
+```
+
+# ZSH
+
+
